9865256a978188d75f5de853c799a62ff974fb22
shahriyardx <mdshahriyaralam552@gmail.com>
2022-10-01 16:49:19 +0600

    Formatted using black and isort

--- eg/cli.py
+++ eg/cli.py
@@ -1,5 +1,7 @@
 import subprocess
+
 import click
+
 
 @click.group(chain=True)
 def cli():
@@ -11,32 +13,34 @@ def cli():
 @click.option("--branch", "-b", help="Branch to push", default=None)
 def push(msg, branch):
     """Push git repo"""
-    subprocess.call(['git', 'add', '.'])
+    subprocess.call(["git", "add", "."])
 
-    if (msg):
-        commit_content = subprocess.check_output(['git', 'commit', '-m', msg])
+    if msg:
+        commit_content = subprocess.check_output(["git", "commit", "-m", msg])
         print(commit_content.decode("UTF-8"))
-    
+
     if branch:
-        push_content = subprocess.check_output(['git','push', '-u', 'origin', branch])
+        push_content = subprocess.check_output(["git", "push", "-u", "origin", branch])
     else:
-        push_content = subprocess.check_output(['git','push'])
-    
+        push_content = subprocess.check_output(["git", "push"])
+
     print(push_content.decode("UTF-8"))
 
 
 @cli.command("init")
-@click.option("--msg", "-m", help="Initial commit message", default="Initialized using easy-git")
+@click.option(
+    "--msg", "-m", help="Initial commit message", default="Initialized using easy-git"
+)
 @click.option("--branch", "-b", help="Branch [main]", default="main")
 @click.option("--origin", "-o", help="Repository url")
 def init(msg, origin, branch):
     """Initialize git repo"""
-    init_content = subprocess.check_output(['git', 'init'])
-    add_content = subprocess.check_output(['git', 'add', '.'])
-    commit_content = subprocess.check_output(['git', 'commit', '-m', msg])
-    branch_content = subprocess.check_output(['git', 'branch', '-M', branch])
-    remote_content = subprocess.check_output(['git', 'remote', 'add', 'origin', origin])
-    
+    init_content = subprocess.check_output(["git", "init"])
+    add_content = subprocess.check_output(["git", "add", "."])
+    commit_content = subprocess.check_output(["git", "commit", "-m", msg])
+    branch_content = subprocess.check_output(["git", "branch", "-M", branch])
+    remote_content = subprocess.check_output(["git", "remote", "add", "origin", origin])
+
     print(init_content.decode("UTF-8"))
     print(add_content.decode("UTF-8"))
     print(commit_content.decode("UTF-8"))
