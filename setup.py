4506debf046b6dfec261542369f8d97c95de3422
shahriyardx <mdshahriyaralam552@gmail.com>
2022-10-01 16:59:50 +0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-import pathlib
+from importlib.util import module_from_spec, spec_from_file_location
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
-here = pathlib.Path(__file__).parent.resolve()
+current_directory = Path(__file__).parent.resolve()
 
-long_description = (here / "README.md").read_text(encoding="utf-8")
+long_description = (current_directory / "README.md").read_text(encoding="utf-8")
+
+vpath = current_directory / "eg" / "__init__.py"
+spec = spec_from_file_location(vpath.name[:-3], vpath)
+mod = module_from_spec(spec)
+spec.loader.exec_module(mod)
 
 setup(
     name="easy-git",
-    version="1.0.0",
+    version=mod.__version__,
     description="Common git operations made simple",  # Optional
     long_description=long_description,
     long_description_content_type="text/markdown",
